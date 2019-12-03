61e79b797201da5cb950bca75c64743038390c5e
Doak Covington <doakcovington@protonmail.com>
2019-12-03 03:31:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
   #return true for valid position on empty board (between 0-8)
-  if index.between?(0,8) && position_taken?(board,index) == true
+  if index.between?(0,8)
     return true
   else
     return false
