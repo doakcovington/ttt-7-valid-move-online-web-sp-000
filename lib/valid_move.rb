54b88b48561888337d19791719cf24f055d0ea40
Doak Covington <doakcovington@protonmail.com>
2019-12-03 03:24:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-
+  #return true for valid position on empty board (between 0-8)
+  if board[index].between?(0,8)
+    return true
+  end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
